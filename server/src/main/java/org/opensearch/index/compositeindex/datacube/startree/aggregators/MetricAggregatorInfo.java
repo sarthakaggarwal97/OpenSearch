2290798e647f8c3cc319ca35e4f065bcf825521f
Sarthak Aggarwal <sarthagg@amazon.com>
2024-07-05 15:18:02 +0530

    fix annotations

--- server/src/main/java/org/opensearch/index/compositeindex/datacube/startree/aggregators/MetricAggregatorInfo.java
+++ server/src/main/java/org/opensearch/index/compositeindex/datacube/startree/aggregators/MetricAggregatorInfo.java
@@ -7,6 +7,7 @@
  */
 package org.opensearch.index.compositeindex.datacube.startree.aggregators;
 
+import org.opensearch.common.annotation.ExperimentalApi;
 import org.opensearch.index.compositeindex.datacube.MetricStat;
 import org.opensearch.index.compositeindex.datacube.startree.aggregators.numerictype.StarTreeNumericType;
 import org.opensearch.index.fielddata.IndexNumericFieldData;
