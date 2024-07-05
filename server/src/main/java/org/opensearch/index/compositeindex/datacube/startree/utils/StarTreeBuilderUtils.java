8a02e55a60246d569c04663d94aa85b122b0a73d
Sarthak Aggarwal <sarthagg@amazon.com>
2024-07-05 15:18:02 +0530

    fix annotations

--- server/src/main/java/org/opensearch/index/compositeindex/datacube/startree/utils/StarTreeBuilderUtils.java
+++ server/src/main/java/org/opensearch/index/compositeindex/datacube/startree/utils/StarTreeBuilderUtils.java
@@ -23,7 +23,6 @@ import java.util.Map;
  *
  * @opensearch.experimental
  */
-@ExperimentalApi
 public class StarTreeBuilderUtils {
 
     private StarTreeBuilderUtils() {}
@@ -34,6 +33,7 @@ public class StarTreeBuilderUtils {
      * Represents a node in a tree data structure, specifically designed for a star-tree implementation.
      * A star-tree node will represent both star and non-star nodes.
      */
+    @ExperimentalApi
     public static class TreeNode {
 
         /**
